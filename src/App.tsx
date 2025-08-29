654e356c6ff1f98e7184d8c6fb4d3479aeba40a1
t11s <greenfeatherhelp@gmail.com>
2025-08-29 12:44:15 -0400

    feat: dvh

--- src/App.tsx
+++ src/App.tsx
@@ -39,7 +39,7 @@ const App = memo(() => {
   const { isAuthLoading, isAuthenticated, affiliation, isAdmin } = useAuthStatus();
 
   return (
-    <Flex direction="column" width="100%" height="100vh">
+    <Flex direction="column" width="100%" height="100dvh">
       <Header
         authenticated={isAuthenticated}
         isAuthLoading={isAuthLoading}
