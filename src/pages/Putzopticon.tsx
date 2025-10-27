ef0d2952d3237f5bd170360f34323c9648823782
t11s <greenfeatherhelp@gmail.com>
2025-10-27 13:41:24 -0400

    feat: better putzopticon sort

--- src/pages/Putzopticon.tsx
+++ src/pages/Putzopticon.tsx
@@ -33,9 +33,15 @@ export const Putzopticon = memo(() => {
             };
           })
           .sort((a, b) => {
+            // Sort by, in order:
+            // 1) color (using COLOR_ORDER)
+            // 2) label (alphabetically)
+            // 3) name (alphabetically)
             const aOrder = COLOR_ORDER.indexOf(a.color);
             const bOrder = COLOR_ORDER.indexOf(b.color);
             if (aOrder !== bOrder) return aOrder - bOrder;
+            const labelCompare = a.label.localeCompare(b.label);
+            if (labelCompare !== 0) return labelCompare;
             return a.name.localeCompare(b.name);
           })
           .map((row) => {
